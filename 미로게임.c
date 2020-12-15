2b17ba53f22d4d88bfb2ce24b1e485efe7c2eedb
yunsoungjun <72550665+yunsoungjun@users.noreply.github.com>
2020-12-15 12:00:32 +0900

    Update 미로게임.c

--- 미로게임.c
+++ 미로게임.c
@@ -12,7 +12,7 @@ int field[10][10] = {
 	{1,0,0,0,0,1,2,0,0,1},
 	{1,0,1,1,0,1,0,1,1,1},
 	{1,2,1,0,0,1,0,0,0,0},
-	{1,1,1,1,1,1,1,1,1,1}        // 미로판
+	{1,1,1,1,1,1,1,1,1,1}        // 배열을 이용한 미로판
 };
 void print_maze() { system("cls");	
 for (int i = 0; i < 10; i++) 
@@ -22,7 +22,7 @@ printf("\n"); }
 printf("점수 : %d", score); }
 void clear() { system("cls");
 printf("점수 : %d\n\n", score);
-printf("%s", "축하합니다! 게임을 깼습니다!");	a = 3;
+printf("%s", "축하합니다! 게임을 클리어 했습니다!");	a = 3;
 }
 void process(int* a) 
 { if (*a == 2) { score++; }	*a = 9; } 
@@ -39,7 +39,7 @@ int main() {
 		printf("%s", "제작\n 윤성준\n 이성현\n 이주현\n 김지성\n 김민석\n\n"); 
 	}
 	if (z == 3) {
-		printf("%s", "게임 설명\n 숫자9를 방향키로 움직여 숫자 2를 먹어 점수를 올리면서 미로를 탈출하자!!\n\n");
+		printf("%s", "게임 설명\n 숫자9를 방향키로 움직여 숫자 2를 획득하여 점수를 올리면서 미로를 탈출하자!!\n\n");
 	}
 	}
 	field[x][y] = 9;	
@@ -47,12 +47,13 @@ int main() {
 	while (a < 4) {
 		if (kbhit()) {
 			key = getch();			
-			if (key == 224 || key == 0) {
+			if (key == 224 || key == 0) {                                        // 아스키코드를 이용한 조작키 설정
 				key = getch();				
 				switch (key) {
 				case 72:					
 				if (x-1 >= 0 && field[x - 1][y] == 1)
-				break;					
+				break;
+				printf("\n ↑ 누르셨습니다. /n");
 				if (x == 0) { clear(); 
 				break; }					
 				field[x][y] = 0;					
@@ -62,7 +63,8 @@ int main() {
 				break;				
 				case 75: 					
 				if (y-1 >= 0 && field[x][y - 1] == 1) 
-				break;					
+				break;
+				printf("\n ← 누르셨습니다. /n");
 				if (y == 0) { clear(); 
 				break; }					
 				field[x][y] = 0;					
@@ -72,7 +74,8 @@ int main() {
 				break;				
 				case 77: 					
 				if (y+1 <= 9 && field[x][y + 1] == 1)
-				break;					
+				break;		
+				printf("\n → 누르셨습니다 /n");
 				if (y == 9) { clear(); 
 				break; }					
 				field[x][y] = 0;					
@@ -82,7 +85,8 @@ int main() {
 				break;				
 				case 80: 					
 					if (x+1 <= 9 && field[x+1][y] == 1) 
-						break;					
+						break;
+					printf("\n ↓ 누르셨습니다 /n");
 					if (x == 9) { clear(); 
 					break; }					
 					field[x][y] = 0;					
